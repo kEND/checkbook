f2f8f3d0e2d643b9e78f8b7ee54af59c7adec692
Ken Barker <ken.barker@gmail.com>
2024-10-31 20:43:44 -0400

    Extract resolve_module/2 and safe_module_concat/1

--- lib/credo/check/refactor/unused_public_functions/calls_collector.ex
+++ lib/credo/check/refactor/unused_public_functions/calls_collector.ex
@@ -1,6 +1,8 @@
 defmodule Credo.Check.Refactor.UnusedPublicFunctions.CallsCollector do
   alias Credo.Check.Refactor.UnusedPublicFunctions.BehaviourMapper
 
+  import Credo.Check.Refactor.UnusedPublicFunctions.ModuleHelper
+
   @moduledoc """
   Collects all function calls from all source files.
 
@@ -180,30 +182,6 @@ defmodule Credo.Check.Refactor.UnusedPublicFunctions.CallsCollector do
     case Map.get(imports, {function, arity}) do
       nil -> nil
       module -> {module, function, arity}
-    end
-  end
-
-  defp safe_module_concat(parts) do
-    parts
-    |> Enum.map(fn
-      {:__MODULE__, _, _} -> "__MODULE__"
-      part when is_atom(part) -> Atom.to_string(part)
-      part when is_binary(part) -> part
-    end)
-    |> Enum.reject(&(&1 == "__MODULE__"))
-    |> Module.concat()
-  end
-
-  defp resolve_module(module_parts, aliases) do
-    case module_parts do
-      [head | tail] ->
-        case Map.get(aliases, head) do
-          nil -> safe_module_concat(module_parts)
-          base_module -> safe_module_concat([base_module | tail])
-        end
-
-      _ ->
-        safe_module_concat(module_parts)
     end
   end
 end
